fed5588d31c38557c29176bc8a6947e5838fc4d7
tanukizzan <51444253+tanukizzan@users.noreply.github.com>
2021-04-20 17:01:20 +0900

    キーワードボックスのアップデート
    キーワードボックスを編集してlocalstorageに保存できるように更新
    それに伴ってキーワードを選択したら検索ボックスに自動入力するように変更
    ダークモード時のデザインを修正

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let wordInput = document.getElementById('window');
 const pulldown = document.getElementById('pulldown');
-const clear = document.getElementById('clear');
+const randomSelect = document.getElementById('random-select');
 const formClear = document.getElementById('form-clear');
-const list = [
+let defaultList = [
   'Photoshop', 'Illustrator', 'Premiere Pro', 'Premiere Rush', 'After Effects', 'Adobe XD', 'CLIP STUDIO', 'Blender', 'Unity', 'iMovie', 'GarageBand', 'Google スライド', 'Google ドキュメント', 'Google スプレッドシート', 'Google サイト', 'PowerPoint', 'Word', 'Excel', 'Slack', 'HTML', 'CSS', 'JavaScript', 'Node.js', 'Python', 'C#', 'PHP', 'WordPress', 'VS Code', 'Git', 'GitHub', 'Linux', 'OpenProcessing', 'Scratch', 'Arduino', 'micro:bit'
 ];
 const button = [
@@ -25,11 +25,27 @@ const button = [
 ];
 const random = document.getElementById('random');
 const hashtag = document.getElementById('hashtag');
+const edit = document.getElementById('edit');
+const dialog = document.getElementById('dialog');
+const closeDialog = document.getElementById('close-dialog');
+const editList = document.getElementById('edit-list');
+const saveList = document.getElementById('save-list');
+const ressetList = document.getElementById('resset-list');
+
 const google = 'https://www.google.co.jp/';
 const query = 'search?q=';
 
-// pulldownに中身を出力
 window.onload = () => {
+  if (localStorage.getItem('localList') === null) {
+    let setJson = JSON.stringify(defaultList);
+    localStorage.setItem('localList', setJson);
+  }
+  pulldownCreate();
+  editList.value = JSON.parse(localStorage.getItem('localList'));
+}
+// pulldownに中身を出力
+function pulldownCreate() {
+  let list = JSON.parse(localStorage.getItem('localList'));
   for (var i=0; i<list.length; i++) {
     let option = document.createElement('option');
     option.value = list[i];
@@ -37,20 +53,46 @@ window.onload = () => {
     pulldown.appendChild(option);
   }
 }
-// pulldownをクリア
-clear.addEventListener('click', function() {
-  pulldown.selectedIndex = 0;
+
+// pulldownを編集してwebstorageに保存
+edit.addEventListener('click', () => {
+  dialog.classList.remove('close');
+  dialog.classList.add('open');
+})
+closeDialog.addEventListener('click', () => {
+  dialog.classList.remove('open');
+  dialog.classList.add('close');
+})
+saveList.addEventListener('click', () => {
+  const listItem = editList.value.split(',');
+  let setJson = JSON.stringify(listItem);
+  localStorage.setItem('localList', setJson);
+  location.reload();
+})
+ressetList.addEventListener('click', () => {
+  if (confirm('キーワードボックスをブラウザから削除します。再読み込みするとデフォルトのリストが設定されます。よろしいですか？')) {
+    localStorage.removeItem('localList');
+    editList.value = '';
+    dialog.classList.remove('open');
+    dialog.classList.add('close');
+    alert('ご利用ありがとうございました！');
+  }
+})
+
+// 選択したpulldownをwordInputに入力
+pulldown.addEventListener('change', () => {
+  if (wordInput.value.length > 0) {
+    wordInput.value = wordInput.value + ' ' + pulldown.value;
+  } else {
+    wordInput.value = pulldown.value;
+  }
+  pulldown.value = 'キーワードボックス';
   wordInput.focus();
 })
 
 // 検索ボックスをクリア
 formClear.addEventListener('click', function() {
   wordInput.value = '';
-  wordInput.focus();
-})
-// ダブルクリックでプルダウンもクリア
-formClear.addEventListener('dblclick', function() {
-  pulldown.selectedIndex = 0;
   wordInput.focus();
 })
 
@@ -60,24 +102,19 @@ hashtag.addEventListener('click', function() {
     return;
   } else if (wordInput.value.length > 0) {
     wordInput.value = '#' + wordInput.value;
-    wordInput.focus();
   } else {
     wordInput.value = '#';
-    wordInput.focus();
   }
+  wordInput.focus();
 })
 
 // google エリア
 // 0 google 検索
 button[0].onclick = () => {
-  if (wordInput.value.length === 0 && pulldown.value === 'null') {
+  if (wordInput.value.length === 0) {
     window.open(google);
-  } else if (wordInput.value.length === 0) {
-    window.open(google + query + encodeURIComponent(pulldown.value));
-  } else if (pulldown.value === 'null') {
-    window.open(google + query + encodeURIComponent(wordInput.value));
   } else {
-    window.open(google + query + encodeURIComponent(pulldown.value) + ' ' + encodeURIComponent(wordInput.value));
+    window.open(google + query + encodeURIComponent(wordInput.value));
   }
 }
 wordInput.addEventListener('keypress', function(event) {
@@ -87,55 +124,39 @@ wordInput.addEventListener('keypress', function(event) {
 });
 // 1 google 画像
 button[1].onclick = () => {
-  if (wordInput.value.length === 0 && pulldown.value === 'null') {
+  if (wordInput.value.length === 0) {
     window.open(google + 'imghp');
-  } else if (wordInput.value.length === 0) {
-    window.open(google + query + encodeURIComponent(pulldown.value) + '&tbm=isch');
-  } else if (pulldown.value === 'null') {
-    window.open(google + query + encodeURIComponent(wordInput.value) + '&tbm=isch');
   } else {
-    window.open(google + query + encodeURIComponent(pulldown.value) + ' ' + encodeURIComponent(wordInput.value) + '&tbm=isch');
+    window.open(google + query + encodeURIComponent(wordInput.value) + '&tbm=isch');
   }
 }
 // 2 google 地図
 button[2].onclick = () => {
-  if (wordInput.value.length === 0 && pulldown.value === 'null') {
+  if (wordInput.value.length === 0) {
     window.open(google + 'maps');
-  } else if (wordInput.value.length === 0) {
-    window.open(google + 'maps/search/' + encodeURIComponent(pulldown.value));
-  } else if (pulldown.value === 'null') {
-    window.open(google + 'maps/search/' + encodeURIComponent(wordInput.value));
   } else {
-    window.open(google + 'maps/search/' + encodeURIComponent(pulldown.value) + ' ' + encodeURIComponent(wordInput.value));
+    window.open(google + 'maps/search/' + encodeURIComponent(wordInput.value));
   }
 }
 // 3 google 翻訳
 button[3].onclick = () => {
   const translate = '?sl=auto&tl=ja&text=';
   const link = 'https://translate.google.co.jp/';
-  if (wordInput.value.length === 0 && pulldown.value === 'null') {
+  if (wordInput.value.length === 0) {
     window.open(link);
-  } else if (wordInput.value.length === 0) {
-    window.open(link + translate + encodeURIComponent(pulldown.value));
-  } else if (pulldown.value === 'null') {
-    window.open(link + translate + encodeURIComponent(wordInput.value));
   } else {
-    window.open(link + translate + encodeURIComponent(pulldown.value) + ' ' + encodeURIComponent(wordInput.value));
+    window.open(link + translate + encodeURIComponent(wordInput.value));
   }
 }
 // 4 youtube
 button[4].onclick = () => {
   const link = 'https://www.youtube.com/';
-  if (wordInput.value.length === 0 && pulldown.value === 'null') {
+  if (wordInput.value.length === 0) {
     window.open(link);
-  } else if (wordInput.value.length === 0) {
-    window.open(link + 'results?q=' + encodeURIComponent(pulldown.value));
   } else if (wordInput.value.match(/^#/)) {
     window.open(link + 'hashtag/' + encodeURIComponent(wordInput.value.slice(1)));
-  } else if (pulldown.value === 'null') {
-    window.open(link + 'results?q=' + encodeURIComponent(wordInput.value));
   } else {
-    window.open(link + 'results?q=' + encodeURIComponent(pulldown.value) + ' ' + encodeURIComponent(wordInput.value));
+    window.open(link + 'results?q=' + encodeURIComponent(wordInput.value));
   }
 }
 
@@ -143,53 +164,37 @@ button[4].onclick = () => {
 // 5 yahoo
 button[5].onclick = () => {
   const link = ['https://www.yahoo.co.jp/', 'https://search.yahoo.co.jp/'];
-  if (wordInput.value.length === 0 && pulldown.value === 'null') {
+  if (wordInput.value.length === 0) {
     window.open(link[0]);
-  } else if (wordInput.value.length === 0) {
-    window.open(link[1] + 'search?p=' + encodeURIComponent(pulldown.value));
-  } else if (pulldown.value === 'null') {
-    window.open(link[1] + 'search?p=' + encodeURIComponent(wordInput.value));
   } else {
-    window.open(link[1] + 'search?p=' + encodeURIComponent(pulldown.value) + ' ' + encodeURIComponent(wordInput.value));
+    window.open(link[1] + 'search?p=' + encodeURIComponent(wordInput.value));
   }
 }
 // 6 bing
 button[6].onclick = () => {
   const link = 'https://www.bing.com/';
-  if (wordInput.value.length === 0 && pulldown.value === 'null') {
+  if (wordInput.value.length === 0) {
     window.open(link);
-  } else if (wordInput.value.length === 0) {
-    window.open(link + query + encodeURIComponent(pulldown.value));
-  } else if (pulldown.value === 'null') {
-    window.open(link + query + encodeURIComponent(wordInput.value));
   } else {
-    window.open(link + query + encodeURIComponent(pulldown.value) + ' ' + encodeURIComponent(wordInput.value));
+    window.open(link + query + encodeURIComponent(wordInput.value));
   }
 }
 // 7 duckduckgo
 button[7].onclick = () => {
   const link = 'https://duckduckgo.com/';
-  if (wordInput.value.length === 0 && pulldown.value === 'null') {
+  if (wordInput.value.length === 0) {
     window.open(link);
-  } else if (wordInput.value.length === 0) {
-    window.open(link + '&q=' + encodeURIComponent(pulldown.value));
-  } else if (pulldown.value === 'null') {
-    window.open(link + '&q=' + encodeURIComponent(wordInput.value));
   } else {
-    window.open(link + '&q=' + encodeURIComponent(pulldown.value) + ' ' + encodeURIComponent(wordInput.value));
+    window.open(link + '&q=' + encodeURIComponent(wordInput.value));
   }
 }
 // 8 wikipedia
 button[8].onclick = () => {
   const link = 'https://ja.wikipedia.org/wiki/';
-  if (wordInput.value.length === 0 && pulldown.value === 'null') {
+  if (wordInput.value.length === 0) {
     window.open(link);
-  } else if (wordInput.value.length === 0) {
-    window.open(link + '特別:検索/' + encodeURIComponent(pulldown.value));
-  } else if (pulldown.value === 'null') {
-    window.open(link + '特別:検索/' + encodeURIComponent(wordInput.value));
   } else {
-    window.open(link + '特別:検索/' + encodeURIComponent(pulldown.value) + ' ' + encodeURIComponent(wordInput.value));
+    window.open(link + '特別:検索/' + encodeURIComponent(wordInput.value));
   }
 }
 
@@ -197,41 +202,29 @@ button[8].onclick = () => {
 // 9 amazon
 button[9].onclick = () => {
   const link = 'https://www.amazon.co.jp/';
-  if (wordInput.value.length === 0 && pulldown.value === 'null') {
+  if (wordInput.value.length === 0) {
     window.open(link);
-  } else if (wordInput.value.length === 0) {
-    window.open(link + 's?k=' + encodeURIComponent(pulldown.value));
-  } else if (pulldown.value === 'null') {
-    window.open(link + 's?k=' + encodeURIComponent(wordInput.value));
   } else {
-    window.open(link + 's?k=' + encodeURIComponent(pulldown.value) + ' ' + encodeURIComponent(wordInput.value));
+    window.open(link + 's?k=' + encodeURIComponent(wordInput.value));
   }
 }
 
 // 10 rakuten
 button[10].onclick = () => {
   const link = ['https://rakuten.co.jp/', 'https://search.rakuten.co.jp/search/mall/'];
-  if (wordInput.value.length === 0 && pulldown.value === 'null') {
+  if (wordInput.value.length === 0) {
     window.open(link[0]);
-  } else if (wordInput.value.length === 0) {
-    window.open(link[1] + encodeURIComponent(pulldown.value));
-  } else if (pulldown.value === 'null') {
-    window.open(link[1] + encodeURIComponent(wordInput.value));
   } else {
-    window.open(link[1] + encodeURIComponent(pulldown.value) + ' ' + encodeURIComponent(wordInput.value));
+    window.open(link[1] + encodeURIComponent(wordInput.value));
   }
 }
 // 11 yahoo shopping
 button[11].onclick = () => {
   const link = 'https://shopping.yahoo.co.jp/';
-  if (wordInput.value.length === 0 && pulldown.value === 'null') {
+  if (wordInput.value.length === 0) {
     window.open(link);
-  } else if (wordInput.value.length === 0) {
-    window.open(link + 'search?p=' + encodeURIComponent(pulldown.value));
-  } else if (pulldown.value === 'null') {
-    window.open(link + 'search?p=' + encodeURIComponent(wordInput.value));
   } else {
-    window.open(link + 'search?p=' + encodeURIComponent(pulldown.value) + ' ' + encodeURIComponent(wordInput.value));
+    window.open(link + 'search?p=' + encodeURIComponent(wordInput.value));
   }
 }
 
@@ -239,72 +232,54 @@ button[11].onclick = () => {
 // 12 twitter
 button[12].onclick = () => {
   const link = 'https://twitter.com/';
-  if (wordInput.value.length === 0 && pulldown.value === 'null') {
+  if (wordInput.value.length === 0) {
     window.open(link);
-  } else if (wordInput.value.length === 0) {
-    window.open(link + query + encodeURIComponent(pulldown.value));
   } else if (wordInput.value.match(/^@/)) {
     window.open(link + encodeURIComponent(wordInput.value.slice(1)));
   } else if (wordInput.value.match(/^#/)) {
     window.open(link + 'hashtag/' + encodeURIComponent(wordInput.value.slice(1)));
-  } else if (pulldown.value === 'null') {
-    window.open(link + query + encodeURIComponent(wordInput.value));
   } else {
-    window.open(link + query + encodeURIComponent(pulldown.value) + ' ' + encodeURIComponent(wordInput.value));
+    window.open(link + query + encodeURIComponent(wordInput.value));
   }
 }
 // 13 instagram
 button[13].onclick = () => {
   const link = 'https://instagram.com/';
-  if (wordInput.value.length === 0 && pulldown.value === 'null') {
+  if (wordInput.value.length === 0) {
     window.open(link);
-  } else if (wordInput.value.length === 0) {
-    alert('現在Instagramのキーワード検索は日本国内で利用できません。解禁されるまではハッシュタグボタンを押してハッシュタグ検索をご利用ください。');
-    // window.open(link + '検索クエリ + encodeURIComponent(pulldown.value));
   } else if (wordInput.value.match(/^@/)) {
     window.open(link + encodeURIComponent(wordInput.value.slice(1)));
   } else if (wordInput.value.match(/^#/)) {
     window.open(link + 'explore/tags/' + encodeURIComponent(wordInput.value.slice(1)));
-  } else if (pulldown.value === 'null') {
-    alert('現在Instagramのキーワード検索は日本国内で利用できません。解禁されるまではハッシュタグボタンを押してハッシュタグ検索をご利用ください。');
-    // window.open(link + '検索クエリ' + encodeURIComponent(wordInput.value));
   } else {
     alert('現在Instagramのキーワード検索は日本国内で利用できません。解禁されるまではハッシュタグボタンを押してハッシュタグ検索をご利用ください。');
-    // window.open(link + '検索クエリ' + encodeURIComponent(pulldown.value) + ' ' + encodeURIComponent(wordInput.value));
+    // window.open(link + '検索クエリ' + encodeURIComponent(wordInput.value));
   }
 }
 // 14 facebook
 button[14].onclick = () => {
   const link = 'https://facebook.com/';
-  if (wordInput.value.length === 0 && pulldown.value === 'null') {
+  if (wordInput.value.length === 0) {
     window.open(link);
-  } else if (wordInput.value.length === 0) {
-    window.open(link + query + encodeURIComponent(pulldown.value));
   } else if (wordInput.value.match(/^@/)) {
     window.open(link + encodeURIComponent(wordInput.value.slice(1)));
   } else if (wordInput.value.match(/^#/)) {
     window.open(link + 'hashtag/' + encodeURIComponent(wordInput.value.slice(1)));
-  } else if (pulldown.value === 'null') {
-    window.open(link + query + encodeURIComponent(wordInput.value));
   } else {
-    window.open(link + query + encodeURIComponent(pulldown.value) + ' ' + encodeURIComponent(wordInput.value));
+    window.open(link + query + encodeURIComponent(wordInput.value));
   }
 }
 // 15 note
 button[15].onclick = () => {
   const link = 'https://note.com/';
-  if (wordInput.value.length === 0 && pulldown.value === 'null') {
+  if (wordInput.value.length === 0) {
     window.open(link);
-  } else if (wordInput.value.length === 0) {
-    window.open(link + 'search?context=note&q=' + encodeURIComponent(pulldown.value));
   } else if (wordInput.value.match(/^@/)) {
     window.open(link + encodeURIComponent(wordInput.value.slice(1)));
   } else if (wordInput.value.match(/^#/)) {
     window.open(link + 'hashtag/' + encodeURIComponent(wordInput.value.slice(1)));
-  } else if (pulldown.value === 'null') {
-    window.open(link + 'search?context=note&q=' + encodeURIComponent(wordInput.value));
   } else {
-    window.open(link + 'search?context=note&q=' + encodeURIComponent(pulldown.value) + ' ' + encodeURIComponent(wordInput.value));
+    window.open(link + 'search?context=note&q=' + encodeURIComponent(wordInput.value));
   }
 }
 
